e8cba582ddc7fd5e8a0828c66d9d0dff57ebc047
Matthew Sah <mattsah2001@gmail.com>
2022-07-28 22:50:54 -0600

    improved styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,7 +17,7 @@ export default function Main() {
     const {name, value} = event.target;
     setMemeData((prevMeme) => ({
       ...prevMeme,
-      [name] : value,
+      [name]: value,
     }))
   }
 
@@ -36,21 +36,24 @@ export default function Main() {
           className="form--input"
           placeholder="Top text"
           onChange={handleChange}
-        ></input>
+        />
         <input
           name="bottomText"
           type="text"
           className="form--input"
           placeholder="Bottom text"
           onChange={handleChange}
-        ></input>
+        />
         <button className="form--button" onClick={setMemeUrl}>
           Get a new meme image 🖼️
         </button>
       </div>
-      <img src={meme.image} alt="meme" className="meme--image"></img>
-      <p>top text {meme.topText}</p>
-      <p>bottom text {meme.bottomText}</p>
+
+      <div className="meme">
+        <img src={meme.image} alt="meme" className="meme--image"></img>
+        <p className="meme--top-text">{meme.topText}</p>
+        <p className="meme--bottom-text">{meme.bottomText}</p>
+      </div>
     </main>
   );
 }
